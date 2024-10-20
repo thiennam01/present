89ab31529494fe2c4db376fd3540bbff434d23fd
thiennam01 <thiennam010405@gmail.com>
2024-10-20 15:27:22 +0700

    Add files via upload

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'Friend!';
+var to = 'Chị em nhà mình!';
 var gift_url = 'http://www.amazon.com/gp/product/B00X4WHP5E/ref=ods_xs_ae_shurl?tag=googhydr-20&hvadid=74652194316&hvpos=1t1&hvexid=&hvnetw=g&hvrand=7560558939874219351&hvpone=&hvptwo=&hvqmt=b&hvdev=c&ref=pd_sl_2vatdqwe3p_b';
 var gift_image_url = 'https://www.intouchsol.com/Content/Libraries//echo-blog-body.png';
 
